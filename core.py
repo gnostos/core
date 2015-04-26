808e69f5ed2c442573c9a056014cf3a90410d4b6
Thibaut Horel <thibaut.horel@gmail.com>
2015-04-25 20:40:02 -0400

    Fix binding for Heroku

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -8,5 +9,6 @@ def hello(name=None):
     return render_template('index.html', name=name)
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app.debug = True
-    app.run()
+    app.run(host="0.0.0.0", port=port)
